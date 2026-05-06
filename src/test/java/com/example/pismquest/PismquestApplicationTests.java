3707e231e34cc496d1f7c799b688ce0f485b0a7b
Caio Reis <caiofreis2005@gmail.com>
2026-05-06 15:25:16 -0300

    init: initialize maven project with wrapper, configuration files, and basic application structure

---
+++ src/test/java/com/example/pismquest/PismquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pismquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PismquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
